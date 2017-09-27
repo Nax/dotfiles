e17e45610d8c05942f59647eebeff211018bf43b
Nax <max@bacoux.com>
2017-09-27 18:26:13 +0200

    Add better comp

--- packages/zsh/zsh/completion.zsh
+++ packages/zsh/zsh/completion.zsh
@@ -1,3 +1,20 @@
 autoload -U colors && colors
 autoload -U compinit && compinit -u
 zstyle ':completion:*' menu select
+zstyle ':completion:*' completer _complete _prefix
+zstyle ':completion:*' add-space true
+
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+zstyle ":completion:*:commands" rehash 1
+
+zstyle ':completion:*:*:*:*:processes' menu yes select
+zstyle ':completion:*:*:*:*:processes' force-list always
+zstyle ':completion:*:*:*:*:processes' command 'ps -A -o pid,user,command'
+zstyle ':completion:*:*:*:*:processes' list-colors "=(#b) #([0-9]#)*=0=${color[green]}"
+zstyle ':completion:*:*:kill:*:processes' command 'ps -e -o pid,user,tty,command'
+
+zstyle ':completion:*:processes-names' command "ps -eo command= | sed 's:\([^ ]*\).*:\1:;s:\(/[^ ]*/\)::;/^\[/d'"
+
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*:warnings' format '%BSorry, no matches for: %d%b'
+zstyle ':completion:*:functions' ignored-patterns '_*'
