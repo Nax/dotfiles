21c9da3055495a572cb667cd33f78fc2335856cf
Nax <max@bacoux.com>
2024-05-19 12:19:15 +0200

    Use vscode as the default editor

--- packages/shell/rc.sh
+++ packages/shell/rc.sh
@@ -43,7 +43,7 @@ export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
 export LS_COLORS="di=1;34:ln=1;35:so=1;31:pi=1;33:ex=1;32:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 export PAGER=$(find_variant most less more)
-export VISUAL=$(find_variant nvim vim vi)
+export VISUAL=$(find_variant code nvim vim vi)
 export EDITOR="$VISUAL"
 
 source "$HOME/.config/shell/alias.sh"
